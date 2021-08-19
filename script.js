473c8110d2a91adb865a0f267d9626a42a824798
Isaiah Meek <50851883+eyezahhhh@users.noreply.github.com>
2021-08-19 15:17:56 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,84 @@
-var playerInfo = [];
-var total = -1;
+var projects = [];
 
-function start() {
-	$.get(serverLoc + "info.php", function(response) {
-		total = response["total"];
+window.onload = function() {
+	$.get("https://web-backend.eyezah.com/eyezah.com/get-projects.php", function(data, status) {
+		document.getElementById("load").style.opacity = 0;
+		setTimeout(function() {
+			document.getElementById("load").remove();
+		}, 300);
 		var i = 0;
 		var html = "";
-		playerInfo = response["players"];
-		while (i < response["players"].length) {
-			html += "<a href=\"stats?user=" + response["players"][i]["username"] + "\"><div><img onLoad=\"this.parentElement.style.opacity = 1\" src=\"https://minotar.net/helm/" + response["players"][i]["uuid"] + "/200\" /><span>" + response["players"][i]["username"] + "</span></div></a>";
+		
+		projects = data;
+		
+		while (i < data.length) {
+			html += "<a href=\"#" + data[i] + "\"><div class=\"project-thumbnail-container\" style=\"background-image: url('https://web-backend.eyezah.com/eyezah.com/get-project-data.php?file=" + data[i] + "-thumbnail.jpg')\"><div class=\"project-thumbnail-fade\"></div><span>" + data[i] + "</span></div></a>";
 			i++;
 		}
-		document.getElementById("current-prize-display").innerHTML = response["total"];
-		document.getElementById("participants-container").innerHTML = html;
-		showPage();
+		document.getElementById("projects-container").innerHTML = html;
+		if (window.location.hash != undefined && window.location.hash != "#" && window.location.hash != "") {
+			var hash = decodeURI(window.location.hash.substring(1));
+			if (projects.includes(hash)) {
+				openProject(hash);
+			} else {
+				window.history.replaceState("", "", "./");
+			}
+		}
 	});
 }
 
-window.onload = function() {
-	start();
+
+var oldHash = window.location.hash;
+window.onpopstate = function(e) {
+	if (window.location.hash != oldHash) {
+		oldhash = window.location.hash;
+		if (window.location.hash == "") {
+			closeProject();
+		} else {
+			var hash = decodeURI(window.location.hash.substring(1));
+			if (projects.includes(hash)) {
+				openProject(hash);
+			} else {
+				window.history.replaceState("", "", "./");
+				closeProject();
+			}
+		}
+	}
+}
+
+window.onhashchange = function() {
+	oldHash = window.location.hash;
+	var hash = decodeURI(window.location.hash.substring(1));
+	if (projects.includes(hash)) {
+		openProject(hash);
+	} else {
+		window.history.replaceState("", "", "./");
+	}
+}
+
+function openProject(id) {
+	document.getElementById("project").scrollTop = 0;
+	document.getElementById("project-banner-image").style.transition = "opacity 0s";
+	document.getElementById("project-banner-image").style.opacity = 0;
+	document.getElementById("project-information").innerHTML = "";
+	document.getElementById("project-banner-image").style.transition = "opacity 0s";
+	document.getElementById("project-information").style.opacity = 0;
+	document.documentElement.style.overflowY = "hidden";
+	setTimeout(function() {
+		document.getElementById("project-banner-image").style.transition = "opacity 0.3s";
+		document.getElementById("project-information").style.transition = "opacity 0.3s";
+		document.getElementById("project-banner-image").src = "https://web-backend.eyezah.com/eyezah.com/get-project-data.php?file=" + id + "-banner.jpg";
+	}, 0);
+	console.log("https://web-backend.eyezah.com/eyezah.com/get-project-data.php?file=" + id + ".txt");
+	$.get("https://web-backend.eyezah.com/eyezah.com/get-project-data.php?file=" + id + ".txt", function(data, status) {
+		document.getElementById("project-information").innerHTML = data;
+		document.getElementById("project-information").style.opacity = 1;
+	});
+	document.getElementById("project").style.left = "0px";
+}
+
+function closeProject() {
+	document.getElementById("project").style.left = "-100%";
+	window.history.pushState("", "", "./");
+	document.documentElement.style.overflowY = "auto";
 }
